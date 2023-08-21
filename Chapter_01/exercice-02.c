ad4144e908e42d968005b0145c6e21ba04ca4c72
Heber-Dsouza <lourenco.h.colares@gmail.com>
2023-08-21 20:25:28 -0300

    exercise 02

---
+++ Chapter_01/exercice-02.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+    int sum = 0;
+    char response[1000] = "", buff[22];
+    for(int i = 1; i <= 200; i++){
+        if(i % 2 == 0){
+            snprintf(buff, 22, "%d,", i);
+            strcat(response, buff);
+            sum += i;
+        }
+    }
+    printf("\n%s\n", response);
+    printf("Soma = %d", sum);
+    return 0;
+}
+
+/* 
+    2. Write a program to print the count of even numbers
+    between 1–200. Also print their sum.
+*/
+
+// Melhoria: retirar o vírgula após o último número
