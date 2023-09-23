2d466aa5e0a1d450c8bbaa2147c423c301b46b0b
Dev-Dsouza <lourenco.h.colares@gmail.com>
2023-09-23 16:53:06 -0300

    New exercices

---
+++ Everyday_Algorithms/findNearestMin.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int* findNearestMin(int *arr, int length, int value){
+  int *min = malloc(sizeof(int)), flag = 0, assigned = 0;
+  for(int i = 0; i < length; i++){
+    if(arr[i] >= value ){
+      flag = 1;
+      if(!assigned){
+        *min = arr[i];
+        assigned = 1;
+      } else if(assigned && arr[i] < *min)
+        *min = arr[i];
+    }
+  }
+  
+  if(!flag)
+    return NULL;
+  else 
+    return min;
+}
+
+int main(){
+  int arr[] = {1, 2, 3}, *nearestMin;
+  nearestMin = findNearestMin(arr, 3, 5);
+  if(nearestMin != NULL){
+    printf("%d", *nearestMin);
+  }
+  free(nearestMin);
+  return 0;
+}
