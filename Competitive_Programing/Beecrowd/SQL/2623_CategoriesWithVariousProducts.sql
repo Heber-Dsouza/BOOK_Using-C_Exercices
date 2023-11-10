4b553d6cc442acaaa4a419562dccae7499cb7aed
Lourenço Heber Colares De Souza <98241441+Heber-Dsouza@users.noreply.github.com>
2023-11-10 09:39:45 -0300

    Update 2623_CategoriesWithVariousProducts.sql

--- Competitive_Programing/Beecrowd/SQL/2623_CategoriesWithVariousProducts.sql
+++ Competitive_Programing/Beecrowd/SQL/2623_CategoriesWithVariousProducts.sql
@@ -1,10 +1,10 @@
 create table categories(
-	id int primary key,
+    id int primary key,
     name varchar(255) not null
 );
 
 create table products (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     amount int not null,
     price decimal(9, 2) not null,
@@ -15,7 +15,7 @@ drop table categories, products;
 
 insert into categories
 values
-	(1, "Superior"),
+    (1, "Superior"),
     (2, "Super Luxury"),
     (3, "Modern"),
     (4, "Nerd"),
@@ -25,7 +25,7 @@ values
 
 insert into products(name, amount, price, id_categories)
 values
-	("Blue Chair", 30, 300.00, 9),
+    ("Blue Chair", 30, 300.00, 9),
     ("Red Chair", 200, 2150.00, 2),
     ("Disney Wardrobe", 400, 829.50, 4),
     ("Blue Toaster", 20, 9.90, 3),
@@ -36,12 +36,12 @@ select * from categories;
 select * from products;
 
 select p.name, c.name from products p join categories c on c.id = p.id_categories
-	where p.amount > 100 
+    where p.amount > 100 
     and c.id not between 4 and 5
     order by c.id asc;
     
 select p.name, c.name from products p join categories c on c.id = p.id_categories
-	where p.amount > 100 
+    where p.amount > 100 
     and c.id < 4
     or p.amount > 100 
     and c.id > 5
@@ -50,12 +50,12 @@ select p.name, c.name from products p join categories c on c.id = p.id_categorie
 -- Response -------------------------------------------------------
     
 select p.name, c.name from products p join categories c on c.id = p.id_categories
-	where p.amount > 100 
+    where p.amount > 100 
     and c.id in(1, 2, 3, 6, 9)
     order by c.id asc;
     
 select p.name, c.name from products p join categories c on c.id = p.id_categories
-	where p.amount > 100 
+    where p.amount > 100 
     and p.id_categories in(1, 2, 3, 6, 9)
     order by c.id asc;
     
