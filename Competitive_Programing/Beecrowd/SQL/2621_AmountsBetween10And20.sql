857886bfc6b3a931d9900de35abf635e92fd9a60
Lourenço Heber Colares De Souza <98241441+Heber-Dsouza@users.noreply.github.com>
2023-11-10 09:38:29 -0300

    Update 2621_AmountsBetween10And20.sql

--- Competitive_Programing/Beecrowd/SQL/2621_AmountsBetween10And20.sql
+++ Competitive_Programing/Beecrowd/SQL/2621_AmountsBetween10And20.sql
@@ -1,5 +1,5 @@
 create table providers(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     street varchar(255) not null,
     city varchar(255) not null,
@@ -7,7 +7,7 @@ create table providers(
 );
 
 create table products (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     amount int not null,
     price decimal(9, 2) not null,
@@ -20,7 +20,7 @@ drop table providers, products;
 
 insert into providers(name, street, city, state)
 values
-	("Ajax SA", "Rua Presidente Castelo Branco", "Porto Alegre", "RS"),
+    ("Ajax SA", "Rua Presidente Castelo Branco", "Porto Alegre", "RS"),
     ("Sansul SA", "Av Brasil", "Rio de Janeiro", "RJ"),
     ("Pr Sheppard Chairs", "Rua do Moinho", "Santa Maria", "RS"),
     ("Elon Electro", "Rua Apolo", "São Paulo", "SP"),
@@ -28,7 +28,7 @@ values
     
 insert into products(name, amount, price, id_providers)
 values
-	("Blue Chair", 30, 300.00, 5),
+    ("Blue Chair", 30, 300.00, 5),
     ("Red Chair", 50, 2150.00, 2),
     ("Disney Wardrobe", 400, 829.50, 4),
     ("Executive Chair", 17, 9.90, 3),
@@ -37,12 +37,12 @@ values
 -- Response -------------------------------------------------------
     
 select p.name from products p join providers pp on p.id_providers = pp.id
-	where p.amount between 10 and 20
+    where p.amount between 10 and 20
     and pp.name like 'P%';
    
 -- alternativa ao join
 select products.name from products, providers where products.id_providers = providers.id
-	and products.amount between 10 and 20
+    and products.amount between 10 and 20
     and providers.name like 'P%';
     
 -- ----------------------------------------------------------------
