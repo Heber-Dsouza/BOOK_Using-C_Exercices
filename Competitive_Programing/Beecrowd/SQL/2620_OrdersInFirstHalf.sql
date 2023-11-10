142cd6c8eeae792e50218026f67bacb261517175
Lourenço Heber Colares De Souza <98241441+Heber-Dsouza@users.noreply.github.com>
2023-11-10 09:37:58 -0300

    Update 2620_OrdersInFirstHalf.sql

--- Competitive_Programing/Beecrowd/SQL/2620_OrdersInFirstHalf.sql
+++ Competitive_Programing/Beecrowd/SQL/2620_OrdersInFirstHalf.sql
@@ -1,5 +1,5 @@
 create table customers(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     street varchar(255) not null,
     city varchar(255) not null,
@@ -8,14 +8,14 @@ create table customers(
 );
 
 create table orders(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     orders_date date not null,
     id_customers int not null, foreign key (id_customers) references customers(id)
 );
 
 insert into customers(name, street, city, state, credit_limit)
 values
-	("Nicolas Diogo Cardoso", "Acesso Um", "Porto Alegre", "RS", 475),
+    ("Nicolas Diogo Cardoso", "Acesso Um", "Porto Alegre", "RS", 475),
     ("Cecília Olivia Rodrigues", "Rua Sizuka Usuy", "Cianorte", "PR", 3170),
     ("Augusto Fernando Carlos Eduardo Cardoso", "Rua Baldomiro Koerich", "Palhoça", "SC", 1067),
     ("Nicolas Diogo Cardoso", "Acesso Um", "Porto Alegre", "RS", 475),
@@ -24,7 +24,7 @@ values
     
 insert into orders(orders_date, id_customers)
 values
-	('2016-05-13', 3),
+    ('2016-05-13', 3),
     ('2016-01-12', 2),
     ('2016-04-18', 5),
     ('2016-09-07', 4),
@@ -37,15 +37,15 @@ select * from orders;
 -- obs.: any of the queries below are working correct.
     
 select c.name, o.id from customers c join orders o on c.id = o.id_customers
-	where date_format(o.orders_date, '%Y%m%d') between date_format('2016-01-01', '%Y%m%d')  and date_format('2016-06-30', '%Y%m%d')
+    where date_format(o.orders_date, '%Y%m%d') between date_format('2016-01-01', '%Y%m%d')  and date_format('2016-06-30', '%Y%m%d')
     order by o.id;
     
 select c.name, o.id from customers c join orders o on c.id = o.id_customers
-	where date(o.orders_date) between '2016-01-01' and '2016-06-30'
+    where date(o.orders_date) between '2016-01-01' and '2016-06-30'
     order by o.id;
     
 select c.name, o.id from customers c join orders o on c.id = o.id_customers
-	where extract(year from o.orders_date) = 2016 and extract(month from o.orders_date) between 1 and 6
+    where extract(year from o.orders_date) = 2016 and extract(month from o.orders_date) between 1 and 6
     order by o.id;
     
 -- ----------------------------------------------------------------
