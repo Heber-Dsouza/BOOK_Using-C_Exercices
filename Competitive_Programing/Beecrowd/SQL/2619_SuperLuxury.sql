291531ccd2fc5e1463aab5b27075f3bc1b3ae8df
Lourenço Heber Colares De Souza <98241441+Heber-Dsouza@users.noreply.github.com>
2023-11-10 09:37:27 -0300

    Update 2619_SuperLuxury.sql

--- Competitive_Programing/Beecrowd/SQL/2619_SuperLuxury.sql
+++ Competitive_Programing/Beecrowd/SQL/2619_SuperLuxury.sql
@@ -1,10 +1,10 @@
 create table categories(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null
 );
 
 create table providers(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     street varchar(255) not null,
     city varchar(255) not null,
@@ -12,7 +12,7 @@ create table providers(
 );
 
 create table products (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     amount int not null,
     price decimal(9, 2) not null,
@@ -22,7 +22,7 @@ create table products (
 
 insert into categories(name)
 values
-	("Super Luxury"),
+    ("Super Luxury"),
     ("Imported"),
     ("Tech"),
     ("Vintage"),
@@ -30,7 +30,7 @@ values
     
 insert into providers(name, street, city, state)
 values
-	("Ajax SA", "Rua Presidente Castelo Branco", "Porto Alegre", "RS"),
+    ("Ajax SA", "Rua Presidente Castelo Branco", "Porto Alegre", "RS"),
     ("Sansul SA", "Av Brasil", "Rio de Janeiro", "RJ"),
     ("South Chairs", "Rua do Moinho", "Santa Maria", "RS"),
     ("Elon Electro", "Rua Apolo", "São Paulo", "SP"),
@@ -38,7 +38,7 @@ values
 
 insert into products(name, amount, price, id_providers, id_categories)
 values
-	("Blue Chair", 30, 300.00, 5, 5),
+    ("Blue Chair", 30, 300.00, 5, 5),
     ("Red Chair", 50, 2150.00, 2, 1),
     ("Disney Wardrobe", 400, 829.50, 4, 1),
     ("Blue Toaster", 20, 9.90, 3, 1),
@@ -47,7 +47,7 @@ values
 select p.name, pp.name, p.price from products p 
 join providers pp on p.id_providers = pp.id
 join categories c on p.id_categories = c.id
-	where p.price > 1000 and c.name = 'Super Luxury';
+where p.price > 1000 and c.name = 'Super Luxury';
         
 /*
 	Our company is looking to make a new contract for the supply of new super luxury products, and for this we need some data of our products.
