0787b3090f04b99f07eafed7972a48d7175946fb
Lourenço Heber Colares De Souza <98241441+Heber-Dsouza@users.noreply.github.com>
2023-11-10 09:36:18 -0300

    Update 2617_ProviderAjaxSA.sql

--- Competitive_Programing/Beecrowd/SQL/2617_ProviderAjaxSA.sql
+++ Competitive_Programing/Beecrowd/SQL/2617_ProviderAjaxSA.sql
@@ -1,5 +1,5 @@
 create table providers(
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(80) not null,
     street varchar(100) not null,
     city varchar(80) not null,
@@ -7,7 +7,7 @@ create table providers(
 );
 
 create table products (
-	id int primary key auto_increment,
+    id int primary key auto_increment,
     name varchar(255) not null,
     amount int not null,
     price int not null,
@@ -15,11 +15,11 @@ create table products (
 );
 
 alter table products
-	modify price decimal(9, 2) not null;
+    modify price decimal(9, 2) not null;
 
 insert into providers(name, street, city, state)
 values
-	("Ajax SA", "Presidente Castelo Branco", "Porto Alegre", "RS"),
+    ("Ajax SA", "Presidente Castelo Branco", "Porto Alegre", "RS"),
     ("Sansul SA", "Av Brasil", "Rio de Janeiro", "RJ"),
     ("South Chairs", "Av Moinho", "Santa Maria", "RS"),
     ("Elon Electro", "Apolo", "São Paulo", "SP"),
@@ -27,14 +27,14 @@ values
     
 insert into products(name, amount, price, id_providers)
 values
-	("Blue Chair", 30, 300.00, 5),
+    ("Blue Chair", 30, 300.00, 5),
     ("Red Chair", 50, 2150.00, 1),
     ("Disney Wardrobe", 400, 829.50, 4),
     ("Blue Toaster", 20, 9.90, 3),
     ("Solar Panel", 30, 3000.25, 4);
     
 select p.name, pp.name from products p join providers pp on p.id_providers = pp.id
-	where pp.name = 'Ajax SA';
+    where pp.name = 'Ajax SA';
     
 /*
 	The financial sector has encountered some problems in the delivery of one of our providers, the delivery of the products does not match the invoice.
